3cec09f81e45bef2c86cc17f7bc43139d3cf3d75
MetaHack <hack@metahack.com>
2011-12-01 21:26:21 -0800

    took out hard-coded debugging output, fixed state-input loop

--- lister2.c
+++ lister2.c
@@ -32,8 +32,6 @@ int main (void) {
       printf("please enter some data : ");
 
       process_line(words[line_count]);
-      printf("...%c%c%c\n", words[line_count][0], words[line_count][1], 
-	     words[line_count][2]);
       line_count++;
     }
   }
@@ -46,7 +44,7 @@ void get_control_character(char * state) {
 
   // grabs characters, makes each one 'state', returning after grabbing newline
 
-  printf("enter q to quit or e to enter a string : ");
+  printf("i = input, p = print list, q = quit : ");
 
   char local_state = 'z';
 
@@ -62,8 +60,6 @@ void get_control_character(char * state) {
     
   }
 
-  
-  printf("state = %c\n", *state);
 }
 
 void process_line(char line[]) {
@@ -76,7 +72,6 @@ void process_line(char line[]) {
     current_char = getchar();
     line[position++] = current_char;
   } while (current_char != '\n');
-  printf("...%d...\n", position);
 
 }
 
